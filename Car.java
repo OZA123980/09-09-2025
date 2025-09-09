79925cba7f6d78f081ddfef3476f133d43bc19f7
OZA_RPL6 <oza8445@gmail.com>
2025-09-09 09:50:33 +0700

    Add files via upload
    Nama: Moh.Fauza Akbar
    No.Absen: 21
    Kelas: X RPL 6 
    
    Tugas Bu Pasha CarTest.java

---
+++ Car.java
@@ -0,0 +1,21 @@
+public class Vehicle {
+    // property class
+    String merk;
+    int milesPerGallon;
+
+    // Constructor Default
+    public Vehicle() {
+        merk = "Toyota";
+        milesPerGallon = 20;
+    }
+
+    // Constructor Parameter
+    public Vehicle(String merk, int milesPerGallon) {
+        this.merk = merk;
+        this.milesPerGallon = milesPerGallon;
+    }
+
+    // getter class
+    // setter class
+    // method class
+}
